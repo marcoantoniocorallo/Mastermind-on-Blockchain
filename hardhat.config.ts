3fdde622983ac2e43756488707913f7d812a8f10
marco <marcoantoniocorallo3@gmail.com>
2024-10-30 18:56:10 +0100

    gas cost

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,8 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import 'solidity-coverage';
+import "hardhat-gas-reporter";
+import { coinmarketcap_key, etherscan_key } from "./APIKey";
 
 const config: HardhatUserConfig = {
   solidity: {
@@ -8,9 +10,16 @@ const config: HardhatUserConfig = {
     settings:{
       optimizer: {
         enabled : true,
-        runs: 1000
+        runs: 200
       }
     }
+  },
+  gasReporter: {
+    enabled: (process.env.REPORT_GAS) ? true : false,
+    L1 : "ethereum",
+    currency: 'EUR',
+    L1Etherscan: etherscan_key,
+    coinmarketcap: coinmarketcap_key,
   }
 };
 
