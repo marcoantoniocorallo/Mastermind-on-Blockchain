d227fa9038f2dc4f7ea8728a18b96d4e4e5c0826
marco <marcoantoniocorallo3@gmail.com>
2024-11-04 17:14:55 +0100

    deployed on Sepolia

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,6 +3,11 @@ import "@nomicfoundation/hardhat-toolbox";
 import 'solidity-coverage';
 import "hardhat-gas-reporter";
 
+const SEPOLIA_PRIVATE_KEY = vars.get("SEPOLIA_PRIVATE_KEY");
+const INFURA_API_KEY = vars.get("INFURA_KEY");
+const EHTERSCAN_KEY = vars.get("ETHERSCAN_KEY");
+const COINMARKETCAP_KEY = vars.get("COINMARKETCAP_KEY");
+
 const config: HardhatUserConfig = {
   solidity: {
     version: "0.8.27",
@@ -17,12 +22,22 @@ const config: HardhatUserConfig = {
     enabled: (process.env.REPORT_GAS) ? true : false,
     L1 : "ethereum",
     currency: 'EUR',
-    L1Etherscan: vars.get("ETHERSCAN_KEY"),
-    coinmarketcap: vars.get("COINMARKETCAP_KEY"),
+    L1Etherscan: EHTERSCAN_KEY,
+    coinmarketcap: COINMARKETCAP_KEY,
     // gasPrice: by default fetched from live network
     gasPrice: 13,
     excludeContracts: ["GameLib"]
   }
+};
+
+module.exports = {
+  solidity: "0.8.27",
+  networks: {
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
+      accounts: [SEPOLIA_PRIVATE_KEY],
+    },
+  },
 };
 
 export default config;
